32e143b389f159aba4f686bf3451e688a9346fd0
unknown <qkrdbals1630@gmail.com>
2019-08-09 12:58:54 +0900

    Upload UAV Deployment

---
+++ UAV Deployment/Assets/Scripts/UserAcademy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class UserAcademy : Academy
+{
+    public override void AcademyReset()
+    {
+        base.AcademyReset();
+    }
+
+    public override void AcademyStep()
+    {
+        base.AcademyStep();
+    }
+}
