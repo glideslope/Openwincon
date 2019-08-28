036609a5c72be4f79f614bc8bc9f715b52dac888
jaejunha <dreamline91@naver.com>
2019-08-28 11:15:15 +0900

    make frame for dqn application

---
+++ Global Dash Streaming/DQN_Application/main.py
@@ -0,0 +1,35 @@
+﻿"""
+Reference: https://github.com/golbin/TensorFlow-Tutorials
+"""
+import sys
+
+import utility as Util
+
+"""
+학습 모드
+"""
+def train_simulation(data):
+	print("Training mode")
+	pass
+
+"""
+재생
+"""
+def play_simulation(data):
+	print("Test mode")
+	pass
+
+if __name__ == "__main__":
+
+	if len(sys.argv) != 2:
+		print("Help > python main.py <mode>")
+		print("Help > <mode> can be 'train' or 'test'") 
+	else:
+		input = sys.argv[1]
+		if input == "train":
+			pass
+			
+		elif input == "test":
+			pass
+		else:
+			print("Check your input")
