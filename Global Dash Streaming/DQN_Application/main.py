853e775b163256349b5fbf7d1da636bec4e8dbf6
jaejunha <dreamline91@naver.com>
2019-08-29 15:08:27 +0900

    make simulation code

--- Global Dash Streaming/DQN_Application/main.py
+++ Global Dash Streaming/DQN_Application/main.py
@@ -1,21 +1,37 @@
 ﻿"""
 Reference: https://github.com/golbin/TensorFlow-Tutorials
 """
+import tensorflow as tf
 import sys
 
 import utility as Util
+from simulation import Simulation
 
 """
 학습 모드
 """
 def train_simulation(data):
 	print("Training mode")
+	session = tf.Session()
+
+	simulation = Simulation(data)
+
+	saver = tf.train.Saver()
+
+	fileWriter = tf.summary.FileWriter('logs', session.graph)
 
 """
 재생 모드
 """
 def test_simulation(data):
 	print("Test mode")
+	session = tf.Session()
+
+	simulation = Simulation(data)
+
+	saver = tf.train.Saver()
+	ckeckpoint = tf.train.get_checkpoint_state('model')
+	saver.restore(session, ckeckpoint.model_checkpoint_path)
 
 if __name__ == "__main__":
 
---
+++ Global Dash Streaming/DQN_Application/simulation.py
@@ -0,0 +1,28 @@
+﻿import math
+
+
+
+class Simulation:
+	def __init__(self, data):
+		self.NUM_UE = data['NUM_UE']
+		self.NUM_AP = data['NUM_AP']
+		self.PERCENT_CONNECT = data['PERCENT_CONNECT']
+		self.VAL_TIMESLOT = data['VAL_TIMESLOT']
+		
+		self.list_rate = data['LIST_RATE']
+		self.NUM_RATE = data['NUM_RATE']
+		
+		"""
+		PSNR 계산
+		"""
+		self.list_PSNR = []
+		for rate in self.list_rate:
+			self.list_PSNR.append(_get_PSNR(rate))
+
+
+	def reset(self):
+		pass
+
+def _get_PSNR(rate):
+	return 6.4157 * math.log10(rate) + 22.27
+	
