286f21866d56bb36d29013e4f825e54aa675cceb
jaejunha <dreamline91@naver.com>
2019-09-04 14:41:16 +0900

    make data for train

--- Global Dash Streaming/DQN_Application/simulation.py
+++ Global Dash Streaming/DQN_Application/simulation.py
@@ -1,6 +1,16 @@
 ﻿import math
+import numpy as np
+import random
 
+# UE와 AP 사이 정보
+SIZE_INFO = 3
+CONST_CONNECTABLE = 0
+CONST_AVAILABLE = 1
+CONST_REQUEST = 2
 
+# Bandwidth 정보
+BW_AVG = 2000
+BW_STD = 500
 
 class Simulation:
 	def __init__(self, data):
@@ -21,8 +31,66 @@ class Simulation:
 
 
 	def reset(self):
-		pass
+		
+		# state 및 구성 정보 초기화
+
+		self.state = np.zeros((self.NUM_UE + 1, self.NUM_AP))
+		self.info = np.zeros((self.NUM_UE, self.NUM_AP, SIZE_INFO))
+		
+		# 연결 가능한 UE-AP 및 이용 가능한 자원 설정
+	
+		# UE-AP 연결
+		for i in range(self.NUM_UE):
+			sum = 0
+			for j in range(self.NUM_AP):
+			
+				# 일정확률(초기 입력 값)로 UE와 AP 연결
+				percent = np.random.randint(100)
+				if percent < self.PERCENT_CONNECT:
+					self.info[i][j][CONST_CONNECTABLE] = 1
+					self.info[i][j][CONST_AVAILABLE] = _get_random_bandwidth()
+
+				sum += self.info[i][j][CONST_CONNECTABLE]
+		
+			# 만일 AP와 연결 가능한 UE가 없을 경우
+			if sum == 0:
+				ap = np.random.randint(self.NUM_AP)
+				self.info[i][ap][CONST_CONNECTABLE] = 1
+				self.info[i][ap][CONST_AVILABLE] = _get_random_bandwidth()
+				
+		# AP-UE 연결
+		for j in range(self.NUM_AP):
+			sum = 0
+			for i in range(self.NUM_UE):
+		
+				# 일정확률(초기 입력 값)로 UE와 AP 연결
+				percent = np.random.randint(100)
+				if percent < self.PERCENT_CONNECT:
+					self.info[i][j][CONST_CONNECTABLE] = 1
+					self.info[i][j][CONST_AVAILABLE] = _get_random_bandwidth()
+
+				sum += self.info[i][j][CONST_CONNECTABLE]
+			
+			# 만일 UE와 연결 가능한 AP가 없을 경우
+			if sum == 0:
+				ue = np.random.randint(self.NUM_UE)
+				self.info[ue][j][CONST_CONNECTABLE] = 1
+				self.info[ue][j][CONST_AVILABLE] = _get_random_bandwidth()
+	
+		for i in range(self.NUM_UE):
+			request = np.random.randint(len(self.list_rate))
+			for j in range(self.NUM_AP):
+				if self.info[i][j][CONST_CONNECTABLE] == 1:
+					self.info[i][j][CONST_REQUEST] = request
+
+
+		
 
 def _get_PSNR(rate):
 	return 6.4157 * math.log10(rate) + 22.27
 	
+def _get_random_bandwidth():
+	bandwidth = random.gauss(BW_AVG, BW_STD)
+	if bandwidth < 0:
+		bandwidth = -bandwidth
+	return bandwidth
